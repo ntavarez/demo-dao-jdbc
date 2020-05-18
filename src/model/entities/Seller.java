ccaa6b26339b9b36bb7c9d6ed6f84bfed7372f4d
Natalia Tavares <natalia.h.tavares@gmail.com>
2020-05-18 16:55:21 -0300

    DepartmentDao implementation and test

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -16,7 +16,6 @@ public class Seller implements Serializable{
 	private Department department;
 	
 	public Seller() {
-		
 	}
 
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
